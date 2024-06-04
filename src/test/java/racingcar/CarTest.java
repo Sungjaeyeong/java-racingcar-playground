af2136a1e926980fe5f0442bc171eb7965a8a9bb
Sungjaeyeong <wodud2587@gmail.com>
2024-06-04 10:56:17 +0900

    test - 자동차_전진시_위치_1증가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -4,13 +4,22 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
     @Test()
     @DisplayName("자동차 이름이 5자를 초과할 경우 예외가 발생해야 한다.")
     void 자동차_이름_5자_이내() {
-        Assertions.assertThatThrownBy(() -> new Car("aaaaaa"))
+        assertThatThrownBy(() -> new Car("aaaaaa"))
                 .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test()
+    @DisplayName("자동차가 전진할 경우 위치가 1만큼 증가한다.")
+    void 자동차_전진시_위치_1증가() {
+        Car car = new Car("kia");
+        car.move();
+        assertThat(car.getLocation()).isEqualTo(1);
     }
 }
