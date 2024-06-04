31b1b5abb0d7d763c0aaccd8f302454100ae80f5
Sungjaeyeong <wodud2587@gmail.com>
2024-06-04 10:57:36 +0900

    feat - 자동차 전진시 위치 1 증가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,11 +2,20 @@ package racingcar;
 
 public class Car {
     String name;
+    int location;
 
     public Car(String name) {
         if (name.length() > 5) {
             throw new RuntimeException();
         }
         this.name = name;
+    }
+
+    public void move() {
+        this.location++;
+    }
+
+    public int getLocation() {
+        return location;
     }
 }
