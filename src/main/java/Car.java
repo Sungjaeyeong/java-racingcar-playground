086d9a8a5d5a93ac1271381db3b1bbc9a75c75b6
Sungjaeyeong <wodud2587@gmail.com>
2024-06-17 22:19:25 +0900

    feat: Car 이름 글자수 검증

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -3,6 +3,8 @@ public class Car {
     private int position;
 
     public Car(String name) {
+        if (name.length() > 5)
+            throw new IllegalArgumentException("자동차의 이름은 5자를 초과할 수 없습니다.");
         this.name = name;
     }
 
--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,19 +1,23 @@
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class CarTest {
     @Test
     void 자동차의_초기_위치는_0이다() {
         Car car = new Car("kia");
-        Assertions.assertThat(car.getPosition()).isEqualTo(0);
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 
     @Test
     void 자동차가_전진할_경우_위치는_1만큼_증가한다() {
         Car car = new Car("kia");
         car.move();
-        Assertions.assertThat(car.getPosition()).isEqualTo(1);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    void 자동차_이름은_5자를_초과할_수_없다() {
+        assertThatThrownBy(() -> new Car("kiaaaa")).isInstanceOf(IllegalArgumentException.class);
     }
 }
