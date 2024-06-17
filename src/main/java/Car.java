882fb120c820c61bdd3474473b4d11c9ca78734a
Sungjaeyeong <wodud2587@gmail.com>
2024-06-17 22:01:18 +0900

    feat: Car 클래스 생성

---
+++ src/main/java/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
---
+++ src/test/java/CarTest.java
@@ -0,0 +1,12 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+    @Test
+    void 자동차의_초기_위치는_0이다() {
+        Car car = new Car("kia");
+        Assertions.assertThat(car.getPosition()).isEqualTo(0);
+    }
+}
